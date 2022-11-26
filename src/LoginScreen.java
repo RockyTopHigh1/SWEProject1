97ff0f5c7db74036654a9f516a82f5b9cf9112b0
RockyTopHigh1 <stephen.sargent0404@gmail.com>
2022-11-25 22:33:58 -0500

    Adding files

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -1,21 +1,29 @@
 import javax.swing.*;
+import java.awt.Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainPizzaFrame extends JFrame{
+public class LoginScreen extends JFrame{
     private JTextField textField1;
     private JPasswordField passwordField1;
     private JButton OKButton;
     private JButton clearButton;
     private JPanel MainPanel;
 
-    public MainPizzaFrame(){
+    public LoginScreen(){
         setContentPane(MainPanel);
         setTitle("Welcome! Please Log in");
         setSize(450, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                Menu myMenu = new Menu();
+            }
+        });
     }
-    public static void main(String[] args){
-        MainPizzaFrame myFrame = new MainPizzaFrame();
-    }
+
 }
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.awt.Menu;
+
+public class Main {
+    public static void main(String[] args){
+        LoginScreen myFrame = new LoginScreen();
+
+
+    }
+}
